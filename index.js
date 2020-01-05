4a5f01a6c96e2ce2809bef388fac92e6b650c948
Julien Vier <vier.julien@googlemail.com>
2020-01-05 11:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase) {
+  var 
+  if (string.toLowerCase(string) === string) {
     return "I can't hear you!";
   }
-  else if (string === uppercase) {
+  else if (string.toUpperCase(string) === string) {
     return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
