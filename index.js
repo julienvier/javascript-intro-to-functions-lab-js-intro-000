1ebd42f5a8855135ba909d9877f4df8ecc1eb9e2
Julien Vier <vier.julien@googlemail.com>
2020-01-05 11:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase);
+  console.log(string.toUpperCase('HELLO'));
 }
 
 function logWhisper(string) {
-  console.log(string.toUpperCase());
+  console.log(string.toLowerCase('hello'));
 }
